ca9282bb67b419ee3d626f322b1ce387fee0d17a
Manuel <mr-manuel@outlook.it>
2024-10-08 07:49:19 +0200

    Broker port missing on reconnect

--- dbus-mqtt-temperature/dbus-mqtt-temperature.py
+++ dbus-mqtt-temperature/dbus-mqtt-temperature.py
@@ -95,8 +95,8 @@ def on_disconnect(client, userdata, rc):
 
     while connected == 0:
         try:
-            logging.warning("MQTT client: Trying to reconnect")
-            client.connect(config["MQTT"]["broker_address"])
+            logging.warning(f"MQTT client: Trying to reconnect to broker {config['MQTT']['broker_address']} on port {config['MQTT']['broker_port']}")
+            client.connect(host=config["MQTT"]["broker_address"], port=int(config["MQTT"]["broker_port"]))
             connected = 1
         except Exception as err:
             logging.error(f"MQTT client: Error in retrying to connect with broker ({config['MQTT']['broker_address']}:{config['MQTT']['broker_port']}): {err}")
@@ -185,7 +185,7 @@ class DbusMqttTemperatureService:
         self._dbusservice.add_path("/ProductId", 0xFFFF)
         self._dbusservice.add_path("/ProductName", productname)
         self._dbusservice.add_path("/CustomName", customname)
-        self._dbusservice.add_path("/FirmwareVersion", "0.0.4 (20240819)")
+        self._dbusservice.add_path("/FirmwareVersion", "0.0.5-dev (20241008)")
         # self._dbusservice.add_path('/HardwareVersion', '')
         self._dbusservice.add_path("/Connected", 1)
 
